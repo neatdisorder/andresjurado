e36e02dd22eee16e341e9784c22dc333714c7ed4
neatdisorder <nachocast92@gmail.com>
2022-10-26 17:01:54 -0300

    Integración de video header.

--- src/components/HomeVideo/index.jsx
+++ src/components/HomeVideo/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { AspectRatio, Box } from "@chakra-ui/react";
+import styles from './styles';
 
 const HomeVideo = ({ src }) => {
   return (
-    <AspectRatio
-      maxHeight={{ base: "50vh", md: "65vh" }}
-      ratio={{ base: 1/1, md: 16/9 }}
-    >
-      <Box as="video" src={src} autoplay="autoplay" muted loop></Box>
+    <AspectRatio {...styles.videoContainer}>
+      <Box as="video" src={src} autoPlay muted loop></Box>
     </AspectRatio>
   );
 };
--- src/components/HomeVideo/styles.js
+++ src/components/HomeVideo/styles.js
@@ -1,5 +1,9 @@
 const styles = {
-
+  videoContainer: {
+    maxHeight: "65vh",
+    ratio: {base: 9/16, md: 16/9},
+    overflow: "hidden",
+  },
 };
 
 export default styles;
